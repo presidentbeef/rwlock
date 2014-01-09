b27eb10f4d111df22935faae5ed2385af54e555d
Justin Collins <justin@presidentbeef.com>
2014-01-09 08:34:39 -0800

    Add simple tests

--- test/test_rwlock.rb
+++ test/test_rwlock.rb
@@ -10,10 +10,13 @@ Thread.abort_on_exception = true
 $DEBUG = true
 
 class TestRWLock < Test::Unit::TestCase
+  attr_reader :lock
+  def setup
+    @lock = RWLock.new
+  end
+
   def test_lots_of_threads
     rw = ReaderWriter.new
-
-    lock = RWLock.new
 
     1000.times do |i|
       t = []
@@ -54,5 +57,21 @@ class TestRWLock < Test::Unit::TestCase
       assert_equal 0, rw.reading
       assert_equal 0, rw.writing
     end
+  end
+
+  def test_read_return_value
+    assert_equal :hello, lock.read_sync { :hello }
+  end
+
+  def test_write_return_value
+    assert_equal :hello, lock.write_sync { :hello }
+  end
+
+  def test_max_return_value
+    assert_equal 10, lock.max
+
+    lock = RWLock.new 100
+
+    assert_equal 100, lock.max
   end
 end
